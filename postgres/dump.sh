07fa4e1521e612e029f30dcd0281c430733368b2
DenisOzindzheDev <denis.ozindzhe@tages.ru>
2024-03-04 15:23:36 +0300

    init psql

---
+++ postgres/dump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TARGET_DB=debug
+TARGET_HOST=localhost
+TARGET_PORT=5432
+TARGET_USER=user
+TARGET_PASSWORD=password
+DUMP_FILE=kong-apim-audit
+
+echo "Restoring database..."
+PGPASSWORD="$TARGET_PASSWORD" pg_restore -v -h  "$TARGET_HOST" -p "$TARGET_PORT" -U "$TARGET_USER" --no-owner --clean --role="$TARGET_USER" -d "$TARGET_DB" $DUMP_FILE
+echo "Database dumped and restored successfully!"
